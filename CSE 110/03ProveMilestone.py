f6a4a2034cafa73c95ba829cd710ac0151f1fc6d
Mark Van Horn <krammark191@gmail.com>
2021-09-30 09:51:45 -0600

    completed 03Prove assignment

--- CSE 110/03ProveMilestone.py
+++ CSE 110/03ProveMilestone.py
@@ -8,6 +8,22 @@ number_of_children = int(input("How many children are there? "))
 number_of_adults = int(input("How many adults are there? "))
 sales_tax_rate = float(input("What is the sales tax rate? "))
 
-# Calculations for subtotal, tax, and total.
+# Calculations for subtotal, tax, and total with .2f precision.
 subtotal = (float(number_of_children) * price_for_child_meal) + (float(number_of_adults) * price_for_adult_meal)
-print(f"\n{subtotal:.2f}")
+print(f"\nSubtotal: ${subtotal:.2f}")
+sales_tax = (sales_tax_rate * subtotal) / 100
+print(f"Sales Tax: ${sales_tax:.2f}")
+total = subtotal + sales_tax
+print(f"Total: ${total:.2f}\n")
+
+# User input for payment. If user input is insufficient, program prompts for additional funds.
+payment_amount = float(input("What is the payment amount? "))
+while payment_amount < total:
+    additional_funds = float(input(f"Your current balance of ${payment_amount:.2f} is not enough, please enter additional funds: "))
+    payment_amount += additional_funds
+    if payment_amount >= total:
+        print(f"Your balance of ${payment_amount:.2f} is sufficient, enjoy your meal!\n")
+
+# Calculation and print statement for change.
+change_amount = payment_amount - total
+print(f"Change: ${change_amount:.2f}")
