e62ae475774453bd1b7ba24d2912805ad1bff41a
Mark Van Horn <krammark191@gmail.com>
2021-10-29 22:05:27 -0600

    added 08 prepare

---
+++ cse110/Week-8/08PrepareCheckpoint.py
@@ -0,0 +1,10 @@
+colors = ["red", "blue", "green", "yellow"]
+for colors in colors:
+    print(colors)
+print("")
+numbers = range(8)
+for numbers in range(8):
+    print(numbers + 1)
+print("")
+for multiple_2 in range(2, 22, 2):
+    print(multiple_2)
