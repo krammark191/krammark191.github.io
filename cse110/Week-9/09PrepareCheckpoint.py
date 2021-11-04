00c003cbb60c515515b7e598d11b5e29661eb296
Mark Van Horn <krammark191@gmail.com>
2021-11-03 19:55:44 -0600

    Added 09Prepare

---
+++ cse110/Week-9/09PrepareCheckpoint.py
@@ -0,0 +1,13 @@
+friends = []
+name = None
+
+while name != "end":
+    name = input("Type the name of a friend: ")
+
+    if name != "end":
+        friends.append(name)
+
+print("\nYour friends are:")
+
+for friend in friends:
+    print(friend)
