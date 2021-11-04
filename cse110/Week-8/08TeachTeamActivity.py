cc299c20843df2814e3bb79a35d449e60c2602b9
Mark Van Horn <krammark191@gmail.com>
2021-11-04 11:36:16 -0600

    added a while loop

--- cse110/Week-8/08TeachTeamActivity.py
+++ cse110/Week-8/08TeachTeamActivity.py
@@ -1,17 +1,27 @@
 import math
 
-# This line prompts the user for the size of the muliplication table.
-user_choice = int(input("How many columns and rows do you want in your multiplication table? "))
+# Setup for a while loop that will allow the user to create more than one table.
+user_input = 'yes'
 
-# This section determines the maximum number that will be multiplied in the table and calculates
-# the necessary spacing needed.
-max_number = user_choice ** 2
-digits = int(math.log10(max_number)) + 1
-
-# This nested for loop creates and prints the muliplication table based on the user input.
-range_size = user_choice + 1
-for row_number in range(1, range_size):
-    for column_number in range(1, range_size):
-        number = row_number * column_number
-        print(f"{number:{digits}}", end=" ")
+# Beginning of while loop containing the table creator.
+while user_input == 'yes':
+    # This line prompts the user for the size of the muliplication table.
+    user_choice = int(input("\nHow many columns and rows do you want in your multiplication table? "))
     print()
+
+    # This section determines the maximum number that will be multiplied in the table and calculates
+    # the necessary spacing needed.
+    max_number = user_choice ** 2
+    digits = len(str(max_number))
+    # digits = int(math.log10(max_number)) + 1 (((OBSELETE)))
+
+    # This nested for loop creates and prints the muliplication table based on the user input.
+    range_size = user_choice + 1
+    for row_number in range(1, range_size):
+        for column_number in range(1, range_size):
+            number = row_number * column_number
+            print(f"{number:{digits}}", end=" ")
+        print()
+
+    # Use input determining whether to loop again or not.
+    user_input = input("\nWould you like to display another table? ").lower()
