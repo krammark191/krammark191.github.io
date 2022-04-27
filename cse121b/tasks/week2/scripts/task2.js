2d25a76c9363f8b8df31ce8fa32507ce504f71bc
Mark Van Horn <krammark191@gmail.com>
2022-04-27 09:32:26 -0600

    Finished week 2 task

--- cse121b/tasks/week2/scripts/task2.js
+++ cse121b/tasks/week2/scripts/task2.js
@@ -3,54 +3,53 @@
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-
+let name = "Mark Van Horn";
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
-
+document.querySelector('#name').textContent = name;
 
 // Step 3: declare and instantiate a variable to hold the current year
-
+let date = new Date();
+let year = date.getFullYear();
 
 // Step 4: place the value of the current year variable into the HTML file
-
+document.querySelector('#year').textContent = year;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-
+let imageName = "images/Profile-Picture.jpg";
 
 // Step 6: copy your image into the "images" folder
 
+
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-
-
+document.querySelector('img').setAttribute('src', imageName);
 
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-
+let favFoods = [" Pizza", " Tacos", " Pasta", " Chicken"];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-
+document.querySelector('#food').textContent = favFoods;
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-
+let newFavFood = " Ice Cream";
 
 // Step 4: add the variable holding another favorite food to the favorite food array
-
+favFoods.push(newFavFood)
 
 // Step 5: repeat Step 2
-
+document.querySelector('#food').textContent = favFoods;
 
 // Step 6: remove the first element in the favorite foods array
-
+favFoods.shift()
 
 // Step 7: repeat Step 2
-
+document.querySelector('#food').textContent = favFoods;
 
 // Step 8: remove the last element in the favorite foods array
-
+favFoods.pop()
 
 // Step 7: repeat Step 2
-
-
-
+document.querySelector('#food').textContent = favFoods;
